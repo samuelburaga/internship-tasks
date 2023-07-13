a0f659b228a40dea8e14e86bedb0be44487e2fdf
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-13 15:25:24 +0300

    Monster class

---
+++ combat-system/monster.ts
@@ -0,0 +1,17 @@
+class Monster implements Entity {
+  constructor(
+    public name: string,
+    public health: number,
+    public strength: number
+  ) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+  attack(target: Entity): void {
+    target.health = target.health - this.strength * 10;
+  }
+  takeDamage(amount: number) {
+    this.health = this.health - amount;
+  }
+}
