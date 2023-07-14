291511c516e543aad7a470ad5298a9bb30be4bc4
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-14 09:38:06 +0300

    Created battle

--- combat-system/start.ts
+++ combat-system/start.ts
@@ -1,2 +1,4 @@
 let hero = new Hero("Superman", 180, 10, EntityType.HERO);
 let monster = new Monster("Thanos", 200, 15, EntityType.MONSTER);
+let battle = new Battle(hero, monster);
+battle.fight();
