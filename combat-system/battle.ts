57aed9229a3518bd6beb9a70ccebe5633a977391
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-14 09:02:44 +0300

    Generic types implemented using Union

--- combat-system/battle.ts
+++ combat-system/battle.ts
@@ -1,7 +1,10 @@
-class Battle<T, U> {
-  player1: T;
-  player2: U;
-  constructor(player1: T, player2: U) {
+class Battle {
+  player1: EntityType.HERO | EntityType.MONSTER;
+  player2: EntityType.HERO | EntityType.MONSTER;
+  constructor(
+    player1: EntityType.HERO | EntityType.MONSTER,
+    player2: EntityType.HERO | EntityType.MONSTER
+  ) {
     this.player1 = player1;
     this.player2 = player2;
   }
