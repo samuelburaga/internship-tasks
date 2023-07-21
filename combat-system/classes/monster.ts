3176a7d37b5d666a01694178dca76087debea7e2
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-21 16:07:06 +0300

    removed type

--- combat-system/classes/monster.ts
+++ combat-system/classes/monster.ts
@@ -1,9 +1,7 @@
-import { Hero } from "../classes/hero";
-import { Entity } from "../interfaces/entity";
 import { EntityType } from "../enums/entity-type";
 import { Player } from "../classes/player";
+
 export class Monster extends Player {
-  type: EntityType;
   constructor(name: string, health: number, strength: number) {
     super(name, health, strength, EntityType.MONSTER);
   }
